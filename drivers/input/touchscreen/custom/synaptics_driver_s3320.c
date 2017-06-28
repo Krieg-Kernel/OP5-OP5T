4f3c5754425041bc4d8963346817b7a23fc8b550
andip71 <andreasp@gmx.de>
2017-06-28 14:05:39 +0200

    input: synaptics: s3320: Stop keypad when touchscreen is in use (configurable)
    When the user's finger is on the touchscreen, don't process any keypad
    button presses, as an unintended button press will cause poor UX.
    
    	Sysfs:
    
    	/sys/module/synaptics_driver_s3320/parameters/no_buttons_during_touch
    
    		Y = buttons will be deactivated as long as touchscreen is touched (default in Boeffla kernel)
    		N = buttons are enabled always (default in stock kernels)
    
    Signed-off-by: andip71 <andreasp@gmx.de>
    [@nathanchance: Changed 1 to true as it's a boolean]

--- drivers/input/touchscreen/custom/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/custom/synaptics_driver_s3320.c
@@ -57,6 +57,8 @@
 #include <linux/project_info.h>
 #include "../synaptics_baseline.h"
 
+#include <linux/moduleparam.h>
+
 /*----------------------Global Define--------------------------------*/
 
 #define TP_UNKNOWN 0
@@ -174,6 +176,11 @@ struct test_header {
 // Button key codes
 #define KEY_BUTTON_LEFT     KEY_BACK
 #define KEY_BUTTON_RIGHT    KEY_APPSELECT
+
+// module parameter
+bool s3320_stop_buttons;
+bool no_buttons_during_touch = true;
+module_param(no_buttons_during_touch, bool, 0644);
 
 /*********************for Debug LOG switch*******************/
 #define TPD_ERR(a, arg...)  pr_err(TPD_DEVICE ": " a, ##arg)
@@ -1493,6 +1500,7 @@ void int_touch(void)
 			MT_TOOL_FINGER, finger_status);
 			input_report_key(ts->input_dev,
 			BTN_TOOL_FINGER, 1);
+			s3320_stop_buttons = no_buttons_during_touch;
 			input_report_abs(ts->input_dev,
 			ABS_MT_POSITION_X, points.x);
 			input_report_abs(ts->input_dev,
@@ -1536,6 +1544,8 @@ void int_touch(void)
 
 	if (finger_num == 0/* && last_status && (check_key <= 1)*/) {
 		input_report_key(ts->input_dev, BTN_TOOL_FINGER, 0);
+		s3320_stop_buttons = false;
+
 #ifndef TYPE_B_PROTOCOL
 		input_mt_sync(ts->input_dev);
 #endif
@@ -1590,7 +1600,7 @@ static void int_key_report_s3508(struct synaptics_ts_data *ts)
 	}
 
 	if (!ts->key_disable) {
-		if ((button_key & BUTTON_LEFT) && !(ts->pre_btn_state & BUTTON_LEFT)) {
+		if ((button_key & BUTTON_LEFT) && !(ts->pre_btn_state & BUTTON_LEFT) && !s3320_stop_buttons) {
 			input_report_key(ts->input_dev, keycode_left, 1);
 			input_sync(ts->input_dev);
 		} else if (!(button_key & BUTTON_LEFT) && (ts->pre_btn_state & BUTTON_LEFT)) {
@@ -1598,7 +1608,7 @@ static void int_key_report_s3508(struct synaptics_ts_data *ts)
 			input_sync(ts->input_dev);
 		}
 
-		if ((button_key & BUTTON_RIGHT) && !(ts->pre_btn_state & BUTTON_RIGHT)) {
+		if ((button_key & BUTTON_RIGHT) && !(ts->pre_btn_state & BUTTON_RIGHT) && !s3320_stop_buttons) {
 			input_report_key(ts->input_dev, keycode_right, 1);
 			input_sync(ts->input_dev);
 		} else if (!(button_key & BUTTON_RIGHT) && (ts->pre_btn_state & BUTTON_RIGHT)) {
